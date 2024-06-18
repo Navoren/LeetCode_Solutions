c28e7e78d487b68fc6d43dafea294060ead0b4bc
Naman Tomar <namantomar1453@gmail.com>
2024-06-18 17:57:07 +0530

    Time: 68 ms (34.66%), Space: 41.2 MB (57.35%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int n = difficulty.size();
+        int m = worker.size();
+
+        priority_queue<pair<int, int>> pq;
+
+        for(int i = 0; i< n; i++){
+            pq.push({profit[i], difficulty[i]});
+        }
+
+        sort(worker.begin(), worker.end(), greater<int>());
+
+        int i = 0;
+        int totalProfit = 0;
+        while(i<m && !pq.empty()){
+            if(pq.top().second > worker[i]) pq.pop();
+            else{
+                totalProfit += pq.top().first;
+                i++;
+            }
+        }
+        return totalProfit;
+    }
+};
