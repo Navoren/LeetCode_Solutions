b0ff5eeda4a99bbe1eb28ccb542c9963f66f293b
Naman Tomar <namantomar1453@gmail.com>
2024-07-11 19:04:27 +0530

    Time: 0 ms (100%), Space: 7.7 MB (52.67%) - LeetHub

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> st;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                int j = st.top();
+                st.pop();
+                reverse(s.begin() + j + 1, s.begin() + i);
+            }
+        }
+
+        string result;
+        for (char c : s) {
+            if (c != '(' && c != ')') {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+};
