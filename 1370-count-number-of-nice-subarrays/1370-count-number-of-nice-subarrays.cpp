71fbc06f12c16df76643c5d97e188a0e5e57fb40
Naman Tomar <namantomar1453@gmail.com>
2024-10-07 12:23:30 +0530

    Time: 80 ms (95.31%), Space: 70.3 MB (37.51%) - LeetHub

---
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int atMost(vector<int>& nums, int goal){
+        if(goal < 0) return 0;
+        int l = 0, r = 0, sum = 0, cnt = 0;
+        while(r<nums.size()){
+            sum += nums[r]%2;
+            while(sum>goal){
+                sum = sum - nums[l]%2;
+                l++;
+            }
+            cnt = cnt + (r-l+1);
+            r++;
+        }
+        return cnt;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums,k) - atMost(nums, k-1);
+    }
+};
