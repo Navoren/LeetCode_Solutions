adddd2452688fd6de7e907529fbad4fc1c1cae44
Naman Tomar <namantomar1453@gmail.com>
2024-11-15 13:00:54 +0530

    Time: 0 ms (100%), Space: 69.6 MB (66.98%) - LeetHub

---
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    static int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n = arr.size();
+
+        int left = 0;
+        while (left + 1 < n && arr[left] <= arr[left + 1]) {
+            left++;
+        }
+        
+        if (left == n - 1) return 0;
+
+        int right = n - 1;
+        while (right > 0 && arr[right - 1] <= arr[right]) {
+            right--;
+        }
+
+        int remove = min(n - left - 1, right);
+
+        int i = 0, j = right;
+        while (i <= left && j < n) {
+            if (arr[i] <= arr[j]) {
+                remove = min(remove, j - i - 1);
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return remove;
+    }
+};
