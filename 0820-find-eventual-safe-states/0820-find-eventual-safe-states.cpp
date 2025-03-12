7326832395c233cabe0d15dd0dfaf94f99fe4343
Naman Tomar <namantomar1453@gmail.com>
2025-03-12 21:48:32 +0530

    Time: 45 ms (54.89%), Space: 66.2 MB (33.22%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    int n;
-    vector<int> degreein;
-    vector< vector<int> > rGraph;
-    vector<int> ans;
-    
-    void init(vector<vector<int>>& G){
-        n = G.size();
-        degreein = vector<int>(n,0);
-        rGraph.resize(n, vector<int>(0) );
-        
-        for (int u=0; u<n; u++){
-            for (int v:G[u]){
-                rGraph[v].push_back(u);
-                degreein[u]++;
-            }
-        }
-    }
-    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        init(graph);
-        queue<int> que;
-        for (int i=0; i<n; i++){
-            if (degreein[i]==0) que.push(i);
-        }
-        while (que.size()){
-            int u = que.front(); que.pop();
-            ans.push_back(u);
-            for (int v:rGraph[u]){
-                degreein[v]--;
-                if (degreein[v]==0) que.push(v);
+        int n = graph.size();
+        vector<int> inDegree(n, 0);
+        vector<vector<int>> adj_reverse(n);
+
+        // Reverse the graph and calculate inDegree
+        for (int i = 0; i < n; i++) {
+            for (auto it : graph[i]) {
+                adj_reverse[it].push_back(i);  // Reverse the edges
+                inDegree[i]++;  // Count incoming edges
             }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+
+        queue<int> q;
+        vector<int> safeNodes;
+
+        // Nodes with 0 inDegree are terminal nodes (safe)
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) q.push(i);
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safeNodes.push_back(node);  // Collect safe node
+
+            for (auto it : adj_reverse[node]) {
+                inDegree[it]--;  // Reduce inDegree
+                if (inDegree[it] == 0) q.push(it);
+            }
+        }
+
+        // Return the sorted list of safe nodes
+        sort(safeNodes.begin(), safeNodes.end());
+        return safeNodes;
     }
 };
