0fe82feaa95cc1506180c2683eacc644fda8e098
Naman Tomar <namantomar1453@gmail.com>
2025-06-06 13:17:14 +0530

    Time: 48 ms (67.82%), Space: 37.2 MB (23.76%) - LeetHub

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        int n = s.size();
+        vector<char> min_suf(n);
+        min_suf[n - 1] = s[n - 1];
+        
+        for (int i = n - 2; i >= 0; --i) {
+            min_suf[i] = min(s[i], min_suf[i + 1]);
+        }
+        
+        string t = "";
+        string result = ""; 
+        int i = 0;
+
+        while (i < n || !t.empty()) {
+            while (i < n && (t.empty() || t.back() > min_suf[i])) {
+                t += s[i++];
+            }
+
+            result += t.back();
+            t.pop_back();
+        }
+
+        return result;
+    }
+};
