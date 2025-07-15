b0f4121519b81243d190827de4a27b35a44f6be6
Naman Tomar <namantomar1453@gmail.com>
2025-07-15 17:01:06 +0530

    Time: 0 ms (100%), Space: 53.6 MB (58.44%) - LeetHub

---
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+const getDecimalValue = head => {
+    let result = head.val;
+    while (head.next) {
+        result = result * 2 + head.next.val;
+        head = head.next;
+    }
+    return result;
+};
