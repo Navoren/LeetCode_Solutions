7a04276cf769202606d3ec34c5ad0846e01c636d
Naman Tomar <namantomar1453@gmail.com>
2025-11-17 12:32:58 +0530

    Time: 243 ms (42.26%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-SELECT a1.machine_id,
-ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS "processing_time" 
-FROM Activity a1 
-INNER JOIN Activity a2
-ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id
-AND a1.activity_type = 'start' AND a2.activity_type = 'end'
-GROUP BY a1.machine_id
+select a.machine_id, 
+round(avg(a2.timestamp - a.timestamp), 3) as processing_time
+from activity as a
+inner join activity as a2
+on a.process_id = a2.process_id 
+and a.machine_id = a2.machine_id
+and a.activity_type = 'start'
+and a2.activity_type = 'end' 
+group by a.machine_id;
