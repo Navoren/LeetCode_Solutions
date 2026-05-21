010c1c1d7cab353bcf1589e9f2c4eddfc1253e30
Naman Tomar <namantomar1453@gmail.com>
2026-05-21 17:23:31 +0530

    Time: 0 ms (100%), Space: 15 MB (25.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,48 +1,42 @@
 class Solution {
-
-private:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>& mat){
-        vis[row][col] = 1;
-        int n = mat.size();
-        int m = mat[0].size();
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, 1, 0, -1};
-        
-        for(int i = 0; i<4; i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if(nrow >=0 && nrow <n && ncol >= 0 && ncol < m 
-            && !vis[nrow][ncol] && mat[nrow][ncol] == 'O'){
-                dfs(nrow, ncol, vis, mat);
-            }
-        }
-    }    
-
 public:
-    void solve(vector<vector<char>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        for(int j = 0; j<m; j++){
-            if(!vis[0][j] && mat[0][j] == 'O'){
-                dfs(0, j, vis, mat);
-            }
-            if(!vis[n-1][j] && mat[n-1][j] == 'O'){
-                dfs(n-1, j, vis, mat);
-            }
-        }
-        for(int i = 0; i<n; i++){
-            if(!vis[i][0] && mat[i][0] == 'O'){
-                dfs(i, 0, vis, mat);
-            }
-            if(!vis[i][m-1] && mat[i][m-1] == 'O'){
-                dfs(i, m-1, vis, mat);
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if(board[i][j] == 'O'){
+                        vis[i][j] = 1;
+                        q.push({i,j});
+                    }
+                }
             }
         }
-        for(int i =0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(!vis[i][j] && mat[i][j] == 'O'){
-                    mat[i][j] = 'X';
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i=0; i<4; i++){
+                int nrow = r + delRow[i];
+                int ncol = c + delCol[i];
+                if(nrow>=0 && ncol >= 0 && nrow <n && ncol <m && vis[nrow][ncol] != 1 && board[nrow][ncol] == 'O'){
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!vis[i][j] && board[i][j] == 'O'){
+                    board[i][j] = 'X';
                 }
             }
         }
