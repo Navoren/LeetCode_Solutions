c0a76c3fa9beb316b0054e7387f903b762fb77cf
Naman Tomar <namantomar1453@gmail.com>
2026-06-12 16:35:47 +0530

    Time: 0 ms (100%), Space: 71.9 MB (14.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        long long sum = 0, maxi = LONG_MIN;
-        for(int i=0; i<nums.size(); i++){
-            sum += nums[i];
-
-            if(sum>maxi){
-                maxi = sum;
-            }
-
-            if(sum<0){
-                sum = 0;
-            }
+        int n = nums.size();
+        int bestEnding = 0;
+        int ans = nums[0];
+        for(int i=0; i<n; i++){
+            int v1 = bestEnding + nums[i];
+            int v2 = nums[i];
+            bestEnding = max(v1, v2);
+            ans = max(ans, bestEnding);
         }
-        return maxi;
+        return ans;
     }
 };
