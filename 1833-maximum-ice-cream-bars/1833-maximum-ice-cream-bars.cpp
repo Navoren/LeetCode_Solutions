1bbeb41eca71e34adfc49f17fa2e710a2ddccd84
Naman Tomar <namantomar1453@gmail.com>
2026-06-21 19:58:10 +0530

    Time: 9 ms (84.16%), Space: 108.9 MB (5.01%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int n = costs.size();
-        int ans = 0;
-        sort(costs.begin(), costs.end());
-        for(int i=0; i<n; i++){
-            if(coins >= costs[i]){
-                ans++;
-                coins = coins - costs[i];
-            }
+        const int MAX_INT = 100001;
+        vector<int> freq(MAX_INT, 0);
+
+        for(auto &it : costs){
+            freq[it]++;
         }
+
+        int ans = 0;
+
+        for(int i=1; i<MAX_INT; i++){
+            if(freq[i] == 0) continue;
+
+            int buy = min(freq[i], coins/i);
+
+            ans += buy;
+            coins -= buy*i;
+
+            if(coins < i) break;
+        }
+
         return ans;
     }
 };
