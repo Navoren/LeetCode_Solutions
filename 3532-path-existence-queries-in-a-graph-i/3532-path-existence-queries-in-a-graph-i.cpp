b95c8dec3024bac7097d6c461f21acd91299ed5b
Naman Tomar <namantomar1453@gmail.com>
2026-07-09 09:26:22 +0530

    Time: 21 ms (55.07%), Space: 229 MB (40.55%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,52 @@
+class DisjointSet{
+  public:
+  vector<int> rank, parent, size;
+  DisjointSet(int n){
+      rank.resize(n+1, 0);
+      parent.resize(n+1);
+      for(int i = 0; i<=n; i++){
+          parent[i] = i;
+      }
+  }
+  
+  int findUPar(int node){
+      if(node == parent[node]) return node;
+      return parent[node] = findUPar(parent[node]);
+  }
+  
+  void unionByRank(int u, int v){
+      int ulp_u = findUPar(u);
+      int ulp_v = findUPar(v);
+      if(ulp_u == ulp_v) return;
+      if(rank[ulp_u] == rank[ulp_v]){
+          parent[ulp_u] = ulp_v;
+      }
+      else if(rank[ulp_v] < rank[ulp_u]){
+          parent[ulp_v] = ulp_u;
+      }
+      else{
+          parent[ulp_v] = ulp_u;
+          rank[ulp_u]++;
+      }
+  }
+};
+
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        DisjointSet ds(n);
+
+        for(int i=1; i<n; i++){
+            if(nums[i] - nums[i-1] <= maxDiff){
+                ds.unionByRank(i, i-1);
+            }
+        }
+
+        vector<bool> ans;
+        for(auto &it: queries){
+            ans.push_back(ds.findUPar(it[0]) == ds.findUPar(it[1]));
+        }
+
+        return ans;
+    }
+};
