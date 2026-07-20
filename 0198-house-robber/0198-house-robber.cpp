cd35806eed315f0b8d825533967c46979ba47471
Naman Tomar <namantomar1453@gmail.com>
2026-07-20 22:21:14 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int maxNonAdj(int n, vector<int>& nums, vector<int>&dp){
-        if(n  == 0){
-            dp[n] = nums[0];
-            return nums[0];
-        }
-        if(n < 0) return 0;
-        if(dp[n] != -1) return dp[n];
-
-        int pick = nums[n] + maxNonAdj(n-2, nums, dp);
-        int nopick = 0 + maxNonAdj(n-1, nums, dp);
-
-        dp[n] = max(pick, nopick);
-        return dp[n];
-    }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        int ans = maxNonAdj(n-1, nums, dp);
-        return ans;
+        vector<int> dp(n+1, -1);
+        return solve(nums, dp, n-1);
+    }
+
+    int solve(vector<int>& nums, vector<int>& dp, int i){
+        if(i < 0) return 0;
+        if(i == 0) return nums[0];
+
+        if(dp[i] != -1) return dp[i];
+
+        int pick = nums[i] + solve(nums, dp, i-2);
+        int notPick = solve(nums, dp, i-1);
+
+        return dp[i] = max(pick, notPick);
+
     }
 };
