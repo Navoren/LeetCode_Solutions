7caeeef3fcadd1a1b1dea8da6fb9540b39369391
Naman Tomar <namantomar1453@gmail.com>
2026-07-20 22:29:55 +0530

    Time: 0 ms (100%), Space: 10.3 MB (62.56%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
-    int maxNonAdj(int start, int end, vector<int>& nums, vector<int>& dp) {
-        if (start > end) return 0;
-        if (dp[start] != -1) return dp[start];
-
-        int pick = nums[start] + maxNonAdj(start + 2, end, nums, dp);
-        int nopick = maxNonAdj(start + 1, end, nums, dp);
-
-        return dp[start] = max(pick, nopick);
-    }
-
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
+
         if (n == 1) return nums[0];
 
-        vector<int> dp1(n, -1);
-        vector<int> dp2(n, -1);
+        int excludeLast = solve(nums, 0, n - 2);
+        int excludeFirst = solve(nums, 1, n - 1);
 
-        int ans1 = maxNonAdj(0, n - 2, nums, dp1);
-        int ans2 = maxNonAdj(1, n - 1, nums, dp2);
+        return max(excludeLast, excludeFirst);
+    }
 
-        return max(ans1, ans2);
+    int solve(vector<int>& nums, int start, int end){
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i= start; i <=end; i++){
+            int pick = nums[i] + prev2;
+            int notPick = prev1;
+
+            int curr = max(pick, notPick);
+
+            prev2 = prev1;
+            prev1 = curr; 
+        }
+        return prev1;
     }
 };
