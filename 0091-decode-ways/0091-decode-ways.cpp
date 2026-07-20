033b65c4111a54bb0198406d60152eea272d6c00
Naman Tomar <namantomar1453@gmail.com>
2026-07-20 23:27:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.04%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int i, string& s, vector<int>& dp) {
+        int n = s.size();
+
+        if (i == n)
+            return 1;
+
+        if (s[i] == '0')
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int oneDigit = solve(i + 1, s, dp);
+
+        int twoDigit = 0;
+        if (i + 1 < n &&
+            (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')))
+        {
+            twoDigit = solve(i + 2, s, dp);
+        }
+
+        return dp[i] = oneDigit + twoDigit;
+    }
+
+    int numDecodings(string s) {
+        vector<int> dp(s.size(), -1);
+        return solve(0, s, dp);
+    }
+};
