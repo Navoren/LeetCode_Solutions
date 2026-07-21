b3ff173b04fe4887063caa65ec3558ea38d47ebe
Naman Tomar <namantomar1453@gmail.com>
2026-07-21 10:45:21 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.76%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    void solve(int m, int n, vector<vector<int>> &dp, vector<vector<int>> &grid) {
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    if (i == 0 && j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        int up = 0, left = 0;
-                        if (i > 0) up = dp[i - 1][j];
-                        if (j > 0) left = dp[i][j - 1];
-                        dp[i][j] = up + left;
-                    }
-                }
-            }
-        }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return solve(m-1, n-1, dp, obstacleGrid);
     }
 
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+    int solve(int row, int col, vector<vector<int>>&dp, vector<vector<int>>& arr){
+        if (row < 0 || col < 0) return 0;
+        if (arr[row][col] == 1) return 0;
+        if(row == 0 && col == 0) return 1;
+        if(dp[row][col] != -1) return dp[row][col];
 
-        if (grid[0][0] == 1 || grid[m-1][n-1] == 1) return 0;
+        int up = solve(row-1, col , dp, arr);
+        int down = solve(row, col-1, dp, arr);
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        solve(m, n, dp, grid);
-        return dp[m - 1][n - 1];
+        return dp[row][col] = up + down;
     }
 };
