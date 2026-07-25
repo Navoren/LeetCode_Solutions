866220a6bce074597928b138cdfb84fa3cd02c92
Naman Tomar <namantomar1453@gmail.com>
2026-07-25 23:19:43 +0530

    Time: 39 ms (72.41%), Space: 113.6 MB (28.79%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+
+        vector<pair<int, double>> cars;
+        for(int i=0; i<n; i++){
+            double time = (double)(target - position[i]) / speed[i];
+            cars.push_back({position[i], time});
+        }
+
+        sort(cars.begin(), cars.end());
+
+        double fleetTime = 0;
+        int fleets = 0;
+
+        for(int i=n-1; i>=0; i--){
+            if(cars[i].second > fleetTime) {
+                fleets++; 
+                fleetTime = cars[i].second;
+            }
+        }
+
+        return fleets;
+    }
+};
