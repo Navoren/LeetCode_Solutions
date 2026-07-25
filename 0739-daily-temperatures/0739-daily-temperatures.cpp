3ce72bff1054fffe5fb7a5d733191e7856668a5d
Naman Tomar <namantomar1453@gmail.com>
2026-07-25 21:56:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        stack<int> st;
+        vector<int> ans;
+        for(int i = temp.size()-1; i >= 0; i--){
+            while(!st.empty() && temp[st.top()] <= temp[i]) st.pop();
+            if(st.empty()) ans.push_back(0);
+            else ans.push_back(st.top() - i);
+            st.push(i);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
