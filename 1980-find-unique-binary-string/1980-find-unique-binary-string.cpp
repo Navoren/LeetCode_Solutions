2a2452feb2121ecb01ea1403010f3fc39a69a935
Naman Tomar <namantomar1453@gmail.com>
2026-07-26 19:23:12 +0530

    Time: 0 ms (100%), Space: 12.3 MB (80.86%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans = "";
+        for(int i=0; i<nums.size(); i++){
+            char ch = nums[i][i];
+            ans += ch == '1' ? '0' : '1';
+        }
+        return ans;
+    }
+};
