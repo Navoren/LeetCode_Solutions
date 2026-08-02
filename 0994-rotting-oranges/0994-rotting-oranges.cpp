eb4d3e96183adc93bf803caf3c53f99ba439094b
Naman Tomar <namantomar1453@gmail.com>
2026-08-02 14:05:55 +0530

    Time: 0 ms (100%), Space: 17.3 MB (17.01%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,8 +3,9 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         queue<pair<pair<int,int>,int>> q;
-        int vis[n][m];
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == 2){
@@ -15,30 +16,33 @@ public:
                 }
             }
         }
+
         int tm = 0;
         int delRow[] = {-1, 0, 1, 0};
         int delCol[] = {0, 1, 0, -1};
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            tm = max(tm , t);
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int time = q.front().second;
+            tm = max(tm , time);
             q.pop();
+
             for(int i=0; i<4; i++){
-                int nrow = r + delRow[i];
-                int ncol = c + delCol[i];
-                if(nrow >=0 && nrow<n && ncol >=0 && ncol <m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                    q.push({{nrow, ncol}, t+1});
-                    vis[nrow][ncol] = 2;
+                int nRow = row + delRow[i];
+                int nCol = col + delCol[i];
+                if(nRow >= 0 && nCol >= 0 && nRow <n && nCol <m && grid[nRow][nCol] == 1 &&vis[nRow][nCol] != 2){
+                    q.push({{nRow, nCol}, time+1});
+                    vis[nRow][nCol] = 2;
                 }
             }
         }
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(vis[i][j] != 2 && grid[i][j] == 1) return -1;
+                if(grid[i][j] == 1 && vis[i][j] != 2) return -1;
             }
         }
+
         return tm;
     }
 };
