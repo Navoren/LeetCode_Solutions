b67f98570e499f884a285fa7fde2888d72c83268
Naman Tomar <namantomar1453@gmail.com>
2026-08-04 12:09:26 +0530

    Time: 0 ms (100%), Space: 17.7 MB (73.13%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>> adj(n);
-        for(auto &it : flights){
+        for(auto &it: flights){
             int u = it[0];
             int v = it[1];
             int wt = it[2];
-            adj[u].push_back({v,wt});
+
+            adj[u].push_back({v, wt});
         }
 
+        queue<
+        pair<int, pair<int,int>>
+        >q;
+
+        q.push({0, {src, 0}});
         vector<int> dist(n, INT_MAX);
         dist[src] = 0;
-        queue<pair<int, pair<int,int>>> q;
-        q.push({0, {src, 0}});
-
         while(!q.empty()){
-            auto it = q.front();
+            int stop = q.front().first;
+            int node = q.front().second.first;
+            int cost = q.front().second.second;
             q.pop();
-            int stops = it.first;
-            int node = it.second.first;
-            int cost = it.second.second;
-
-            if(stops > k ) continue;
-            for(auto iter : adj[node]){
-                int adjNode = iter.first;
-                int edjW = iter.second;
-
-                if(edjW + cost < dist[adjNode] && stops <= k){
-                    dist[adjNode] = edjW + cost;
-                    q.push({stops + 1, {adjNode, cost + edjW}});
+            if(stop > k) continue;
+            for(auto &[v, wt] : adj[node]){
+                if(wt + cost < dist[v] && stop <= k){
+                    dist[v] = wt + cost;
+                    q.push({stop +1, {v, dist[v]}});
                 }
             }
         }
+
 
         if(dist[dst] == INT_MAX) return -1;
         return dist[dst];
