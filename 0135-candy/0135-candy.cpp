d5bbd73c56b0b33c9a021d987f62d1a7b2221a1b
Naman Tomar <namantomar1453@gmail.com>
2026-08-28 18:46:49 +0530

    Time: 7 ms (12.53%), Space: 71.4 MB (12.29%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,8 +3,6 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> candies(n,1);
-        int cnt = 0;
-
         for(int i=1; i<n; i++){
             if(ratings[i] > ratings[i-1])
             candies[i] = candies[i-1] + 1;
@@ -12,8 +10,9 @@ public:
         for(int i=n-1; i > 0; i--){
             if(ratings[i-1] > ratings[i])
             candies[i-1] = max(candies[i] + 1, candies[i-1]);
-            cnt += candies[i-1];
         }
-        return cnt + candies[n-1];
+
+        int ans = accumulate(candies.begin(), candies.end(), 0);
+        return ans;
     }
 };
