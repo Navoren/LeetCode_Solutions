8f8ecef167a590c3f2dc4fff1b8627198d6ad5e8
Naman Tomar <namantomar1453@gmail.com>
2026-09-03 14:10:04 +0530

    Time: 25 ms (78.11%), Space: 123.9 MB (65.68%) - LeetHub

---
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> dp(n);
+        deque<int> dq;
+
+        int ans = INT_MIN;
+        for(int i=0; i<n; i++){
+
+            while(!dq.empty() && dq.front() < i-k){
+                dq.pop_front();
+            }
+
+            if(dq.empty()){
+                dp[i] = nums[i];
+            }else{
+                dp[i] = nums[i] + max(0, dp[dq.front()]);
+            }
+
+            ans = max(ans, dp[i]);
+
+            while(!dq.empty() && dp[dq.back()] <= dp[i]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        return ans;
+    }
+};
